2e54547067b01e5a8123ca77f488690b7c1164a2
Johnathan104 <jamesjayawara@gmail.com>
2022-05-03 08:06:04 -0500

    search bar added

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -4,6 +4,7 @@ import Pokemon from './Pokemon.js'
 import axios from 'axios'
 import { useState } from 'react'
 
+
 const Entry=()=>{
     //get the url paramaters
     const pokemon = useParams().pokemon
@@ -32,40 +33,43 @@ const Entry=()=>{
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
              .then(async response=>{
                  // get the description for the pokemon entry and if it has gender difference
-                 await axios.get(response.data.species.url)
-                 .then(res=>{
-                     if(res.data.has_gender_differences)
-                     {
-                        setDiff(<button className='btn' onClick={()=>{changeGender()}}>change gender</button>)
-                     }
-                     setDesc(res.data.flavor_text_entries[1].flavor_text+'\n'+ res.data.flavor_text_entries[3].flavor_text)  
-                 })
-                 .catch(err=>{
-                     setDesc('description not found')
-                 })
+                 if(abilityDescription.length !== abilities.length){
+                    await axios.get(response.data.species.url)
+                    .then(res=>{
+                        if(res.data.has_gender_differences)
+                        {
+                            setDiff(<button className='btn' onClick={()=>{changeGender()}}>change gender</button>)
+                        }
+                        setDesc(res.data.flavor_text_entries[1].flavor_text+'\n'+ res.data.flavor_text_entries[3].flavor_text)  
+                    })
+                    .catch(err=>{
+                        setDesc('description not found')
+                    })
+                 }
                  let ablty = []
                  let dsc = []
                  let length = response.data.abilities.length
-
-                 for(let i = 0;i<length;i++){
-                     if(abilityDescription < length){
-                         await axios.get(response.data.abilities[i].ability.url)
-                         .then(res=>{
-                             for(let x=0; x< res.data.effect_entries.length;x++)
-                             {
-                                 if(res.data.effect_entries[x].language.name === "en"){
-                                     dsc.push(res.data.effect_entries[x].effect)
-                                     setAbilityDescription(dsc)
-                                 }
-                             }
-                         })
-                         .catch(err=>console.log(err))
-                     }
-
-                     ablty.push(<p><b>{response.data.abilities[i].ability.name}</b>: {abilityDescription[i]}</p>)
-                    
+                 if(abilities.length != length){
+                    for(let i = 0;i<length;i++){
+                        if(abilityDescription < length){
+                            await axios.get(response.data.abilities[i].ability.url)
+                            .then(res=>{
+                                for(let x=0; x< res.data.effect_entries.length;x++)
+                                {
+                                    if(res.data.effect_entries[x].language.name === "en"){
+                                        dsc.push(res.data.effect_entries[x].effect)
+                                        setAbilityDescription(dsc)
+                                    }
+                                }
+                            })
+                            .catch(err=>console.log(err))
+                        }
+   
+                        ablty.push(<p><b>{response.data.abilities[i].ability.name}</b>: {abilityDescription[i]}</p>)
+                       
+                    }
+                    setAbilities(ablty)
                  }
-                 setAbilities(ablty)
              })
              .catch((err)=>{
                  setAbilities([<p>abilities not found</p>])
@@ -74,23 +78,32 @@ const Entry=()=>{
                  console.log(err)
              })
     return(
-            <div className='entry'>
-                <Pokemon isShiny={isShiny} isFemale={isFemale} inMain={false}id={pokemon}/>
-                <div className='pokemon-entry-big'>
-                    <h1>{pokemon}</h1>
-                    <h2>Description:</h2>
-                    <p>{desc}</p>
-                    <div className='ability-entry'>
-                        <h2>Abilities :</h2>
-                        {abilities}
+            <div style={{display:'flex', flexDirection:'row', height:'100%', width:'80%'}}>
+                <div className='entry'>
+                    {desc.length !== 0?
+                    <div className='entry-found'>
+                        <Pokemon isShiny={isShiny} isFemale={isFemale} inMain={false}id={pokemon}/>
+                        <div className='pokemon-entry-big'>
+                            <h1>{pokemon}</h1>
+                            <h2>Description:</h2>
+                            <p>{desc}</p>
+                            <div className='ability-entry'>
+                                <h2>Abilities :</h2>
+                                {abilities}
+                            </div>
+                            <h2>Sprite settings:</h2>
+                            <div className='sprite-setting'>
+                                {diffFemale}
+                                <br/>
+                                <button className='shiny-btn' onClick={()=>{shinyHandle()}}>shiny</button>
+                            </div>
+                        </div>
                     </div>
-                    <h2>Sprite settings:</h2>
-                    <div className='sprite-setting'>
-                        {diffFemale}
-                        <br/>
-                        <button className='shiny-btn' onClick={()=>{shinyHandle()}}>shiny</button>
-                    </div>
+                    :
+                    <h1 style={{ alignSelf:'center',textAlign:'center', width:'100%'}}>Not Found</h1>
+                    }
                 </div>
-            </div>)
+            </div>
+            )
 }
 export default Entry
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Pokemon from './Pokemon'
 import axios from 'axios';
-
+import {Link} from 'react-router-dom'
 
 class Main extends React.Component{
     constructor(){
@@ -35,6 +35,7 @@ class Main extends React.Component{
             })
         }
         this.state ={
+            search:'',
             entries:[],
         } 
     }
@@ -42,10 +43,28 @@ class Main extends React.Component{
         //call the function to get pokemons when the app starts
         this.getPokes()
     }
+    searchHandle(event){
+        this.setState({search:event.target.value})
+        console.log(this.state.search)
+    }
+
     render(){
         return(
-                <div className='pokemons'>
-                    {this.state.entries}
+                <div className='main'>
+                    <div className='search'>
+                        <input className='search-bar' type='text' onChange={(event)=>this.searchHandle(event)}/>
+                        {this.state.search.length!==0?
+                            <Link to={this.state.search.toLowerCase()}>
+                                <button className='search-btn'>
+                                    search
+                                </button>
+                            </Link>
+                            :
+                            <button className='search-btn'>search</button>}
+                    </div>
+                    <div className='pokemons'>
+                        {this.state.entries}
+                    </div>
                 </div>
         )
     }
